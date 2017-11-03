69151ac78d3a2e49a03933890df68b7d6f1fc62e
Joseph Casiano <josephcasiano@gmail.com>
2017-11-03 01:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   var lowercase = 'hello'
   var lowercased = "I can't hear you!"
-  lowercase.toLowerCase() === lowercased
+  lowercase.toLowerCase() === lowercased.toLowerCase()
   return string.toLowerCase()
 }
